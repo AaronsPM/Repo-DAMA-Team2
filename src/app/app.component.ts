87916cc1c2ffce2356f26441a6585309263df67e
Rokket121 <sergibaezabaeza@gmail.com>
2024-05-01 23:47:14 +0200

    primeros pasos Sergi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { PayComponent } from './componentes/pay/pay.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
