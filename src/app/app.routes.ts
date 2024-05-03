3bb0fcfcc1b0b32ed2950f24365944c77dd05b00
AaronPM <aaron.pieramarti@cieep.com>
2024-05-03 20:32:49 +0200

    Error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ContactoComponent } from './component/contacto/contacto.component';
 import { ProductosComponent } from './component/productos/productos.component';
 import { CatalogoComponent } from './component/catalogo/catalogo.component';
 import { LoginComponent } from './component/login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'productos', component: ProductosComponent},
     { path: 'catalogo', component: CatalogoComponent},
     { path: 'inicio', component: LoginComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent}
 ];
 
