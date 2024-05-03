cf71f7573f40143c26a4f9f889c063aed315d28c
AaronPM <aaron.pieramarti@cieep.com>
2024-05-03 20:51:18 +0200

    Navbar estático

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { ContactoComponent } from './component/contacto/contacto.component';
-import { ProductosComponent } from './component/productos/productos.component';
-import { CatalogoComponent } from './component/catalogo/catalogo.component';
-import { LoginComponent } from './component/login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+    import { Routes } from '@angular/router';
+    import { HomeComponent } from './component/home/home.component';
+    import { ContactoComponent } from './component/contacto/contacto.component';
+    import { ProductosComponent } from './component/productos/productos.component';
+    import { CatalogoComponent } from './component/catalogo/catalogo.component';
+    import { LoginComponent } from './component/login/login.component';
+    import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'contacto', component: ContactoComponent},
-    { path: 'productos', component: ProductosComponent},
-    { path: 'catalogo', component: CatalogoComponent},
-    { path: 'inicio', component: LoginComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
-    { path: '**', component: PageNotFoundComponent}
-];
+    export const routes: Routes = [
+        { path: 'home', component: HomeComponent},
+        { path: 'contacto', component: ContactoComponent},
+        { path: 'productos', component: ProductosComponent},
+        { path: 'catalogo', component: CatalogoComponent},
+        { path: 'inicio', component: LoginComponent},
+        { path: '', redirectTo: '/home', pathMatch: 'full'},
+        { path: '**', component: PageNotFoundComponent}
+    ];
 
