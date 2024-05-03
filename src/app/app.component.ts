c0ddbd81275929b13aa69bc62b23bc33a352d232
Aaron <aaron.pieramarti@cieep.com>
2024-05-03 10:55:10 +0200

    Enrutamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { ProductosComponent } from './component/productos/productos.component';
+import { CatalogoComponent } from './component/catalogo/catalogo.component';
+import { PayComponent } from './component/pay/pay.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, ContactoComponent, HomeComponent, LoginComponent, ProductosComponent, CatalogoComponent, PayComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ContactoComponent } from './component/contacto/contacto.component';
 import { ProductosComponent } from './component/productos/productos.component';
+import { CatalogoComponent } from './component/catalogo/catalogo.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'contacto', component: ContactoComponent},
     { path: 'productos', component: ProductosComponent},
+    { path: 'catalogo', component: CatalogoComponent},
+    { path: 'inicio', component: LoginComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
